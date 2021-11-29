23d6a13b0a318de78d8fea1e88e52da1799aa055
nataytali <nataliadivchur@gmail.com>
2021-11-29 12:48:49 +0200

    Initial

---
+++ PizzaStoreEF/PizzaDb.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+namespace PizzaStore.PizzaDb
+{
+    class PizzaDb : DbContext
+    {
+    public PizzaDb(DbContextOptions options) : base(options) { }
+    public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
